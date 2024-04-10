df924c37a4f10048f543abe8639c27d003953c72
Lerchenfeld, Leonardo <leonardo.vonlerchenfeld@guentner.com>
2024-04-10 15:16:28 +0200

    bugfix quotes for homeHtmlToInsertIntoMainPage

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -113,7 +113,7 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", '"' + chosenCategoryShortName + '"');
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
@@ -148,7 +148,7 @@ dc.loadMenuCategories = function () {
 dc.loadMenuItems = function (categoryShort) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-    menuItemsUrl + categoryShort,
+    menuItemsUrl + categoryShort + ".json",
     buildAndShowMenuItemsHTML);
 };
 
